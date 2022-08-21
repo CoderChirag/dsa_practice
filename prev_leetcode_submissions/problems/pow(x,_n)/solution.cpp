46429ae4b7aea2f598f1e680d8cd31fd39999f7d
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-08-21 12:27:29 +0000

    Sync LeetCode submission - Pow(x, n) (cpp)

--- prev_leetcode_submissions/problems/pow(x,_n)/solution.cpp
+++ prev_leetcode_submissions/problems/pow(x,_n)/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-   double myPow(double x, int n) {
-        
-        double ans = 1.0;
-        long long nn = n;
-        
-        if(n < 0) nn = nn * -1;
-        
-        while(nn){
-            if(nn % 2){
-                nn = nn - 1;
-                ans = ans * x;
-            }else{
-                nn = nn / 2;
-                x = x * x;
+    double myPow(double x, int n) {
+        double res = 1.0;
+        bool flag = true;
+        if(n < 0 ){
+            flag = false;
+            n = abs(n);
+        };
+        while(n > 0){
+            if(n & 1){
+                res *= x;
             }
+            x *= x;
+            n >>= 1;
         }
-        if(n<0) ans = (double)(1.0) / (double)(ans);
-        return ans;
+        if(!flag) res = 1/res;
+        return res;
     }
 };
