4a48bfab2c653161253bb33358da899f9c4709a4
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-09-20 16:48:59 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- prev_leetcode_submissions/problems/unique_paths/solution.cpp
+++ prev_leetcode_submissions/problems/unique_paths/solution.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    double fact(int n){
-        double res = 1;
-        for(int i=2; i<=n; i++){
-            res *= i;
-        }
-        return res;
-    }
     int uniquePaths(int m, int n) {
-        // int N = n+m-2;
-        // int r = m-1;
-        // return (int)(fact(N)/(fact(r)*fact(N-r)));
         int N = n + m - 2;
-            int r = m - 1; 
-            double res = 1;
-            
-            for (int i = 1; i <= r; i++)
-                res = res * (N - r + i) / i;
-            return (int)res;
+        int r = m - 1;
+        double res = 1;
+        for(int i=1; i<=r; i++){
+            res = res * (N-r+i)/i;
+        }
+        return (int)res;
     }
 };
