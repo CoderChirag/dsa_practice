81c42c3edc693a832e37d8a55b79c788d5d976a7
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-10-02 12:27:07 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (cpp)

--- prev_leetcode_submissions/problems/best_time_to_buy_and_sell_stock_ii/solution.cpp
+++ prev_leetcode_submissions/problems/best_time_to_buy_and_sell_stock_ii/solution.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minCP = prices[0], profit = 0;
-        for(int i=1; i<prices.size(); i++){
-            if(prices[i] > minCP){
-                profit += prices[i] - minCP;
-                minCP = prices[i];
+        int profit = 0;
+        for(int i=0; i<prices.size()-1; i++){
+            if(prices[i] < prices[i+1]){
+                profit += prices[i+1] - prices[i];
             }
-            minCP = min(minCP, prices[i]);
         }
         return profit;
     }
