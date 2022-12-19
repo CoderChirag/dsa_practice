a74fd1a5ad58852335ad13aedb4a39e0d2fda72f
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-12-19 11:24:01 +0530

    Time: 35 ms (16.73%), Space: 46.5 MB (19.80%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,16 +1,30 @@
-class Solution {
-    public static int longestCommonSubsequence(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        for (int i = 0; i < s1.length(); ++i)
-        for (int j = 0; j < s2.length(); ++j)
-        if (s1.charAt(i) == s2.charAt(j)) dp[i + 1][j + 1] = 1 + dp[i][j];
-        else dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
-        for (int i = 0; i < dp.length; i++) {
-        for (int j = 0; j < dp[0].length; j++) {
-        System.out.print(" "+dp[i][j]);
+// Recursive Solution - Time Limit Exceeded
+
+// class Solution { 
+//     public int lcs(String t1, String t2){
+//         if(t1.length() == 0 || t2.length() == 0) return 0;
+//         if(t1.charAt(0) == t2.charAt(0)) return 1 + lcs(t1.substring(1, t1.length()), t2.substring(1, t2.length()));
+        
+//         return Math.max(lcs(t1.substring(1, t1.length()), t2), lcs(t1, t2.substring(1, t2.length())));
+//     }
+    
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         return lcs(text1, text2);
+//     }
+// }
+
+
+// DP - Tabular Form
+class Solution {     
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length(), m = text2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
         }
-        System.out.println();
-        }
-        return dp[s1.length()][s2.length()];
+        return dp[n][m];
     }
 }
