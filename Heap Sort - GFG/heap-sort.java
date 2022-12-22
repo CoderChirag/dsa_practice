39e7078ce6d2f87163a4f3f7b4d905c82074f579
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-12-22 11:51:12 +0530

    Added solution - LeetHub

---
+++ Heap Sort - GFG/heap-sort.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.util.*;
+class Heap_Sort
+{
+    void printArray(int arr[],int n)
+    {
+        //int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        Scanner sc  = new Scanner(System.in);
+        Heap_Sort hs = new Heap_Sort();
+        int arr[] = new int[1000000];
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int n = sc.nextInt();
+            for(int i=0;i<n;i++)
+                arr[i] = sc.nextInt();
+                
+            Solution ob=new Solution();
+            ob.heapSort(arr,n);
+            hs.printArray(arr,n);
+            T--;
+        }
+    }
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)
+    {
+        // Your code here
+        for(int i=(n-2)/2; i>=0; i--){
+            heapify(arr, n, i);
+        }
+    }
+ 
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)
+    {
+        // Your code here
+        int max = i, lt = 2*i+1, rt = 2*i+2;
+        if(lt < n && arr[lt] > arr[max])
+            max = lt;
+        if(rt < n && arr[rt] > arr[max])
+            max = rt;
+            
+        if(max != i){
+            int temp = arr[max];
+            arr[max] = arr[i];
+            arr[i] = temp;
+            heapify(arr, n, max);
+        }
+    }
+    
+    //Function to sort an array using Heap Sort.
+    public void heapSort(int arr[], int n)
+    {
+        //code here
+        buildHeap(arr, n);
+        for(int i=0; i<n; i++){
+            int max = arr[0];
+            arr[0] = arr[n-i-1];
+            arr[n-i-1] = max;
+            heapify(arr, n-i-1, 0);
+        }
+    }
+ }
+ 
+ 
