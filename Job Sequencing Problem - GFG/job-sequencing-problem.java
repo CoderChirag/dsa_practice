381cde3b0261019a98e25144d63a930fecb00a28
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-12-23 15:45:07 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        Arrays.sort(arr, (j1, j2)-> Integer.compare(j1.deadline, j2.deadline));
+        PriorityQueue<Job> q = new PriorityQueue<>((j1, j2) -> Integer.compare(j2.profit, j1.profit));
+        int res[] = new int[2];
+        for(int i=arr.length-1; i>=0; i--){
+            int slots = 0;
+            if(i == 0) slots = arr[i].deadline;
+            else slots = arr[i].deadline - arr[i-1].deadline;
+            
+            q.offer(arr[i]);
+            while(slots > 0 && q.size() > 0){
+                res[0]++;
+                res[1] += q.poll().profit;
+                slots--;
+            }
+        }
+        return res;
+    }
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
