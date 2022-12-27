2131c378d6ae15738f3e11a4f42e0ce1ea9fb49c
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-12-27 10:41:14 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -32,24 +32,54 @@ class GFG {
 // } Driver Code Ends
 
 // DFS Based Approach
-class Solution {
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj, boolean visited[], int v, int parent){
-        visited[v] = true;
-        for(int e : adj.get(v)){
-            if(!visited[e]){
-                if(dfs(adj, visited, e, v)) return true;
-            }
-            else if(e != parent) return true;
-        }
-        return false;
-    }
+// class Solution {
+//     public boolean dfs(ArrayList<ArrayList<Integer>> adj, boolean visited[], int v, int parent){
+//         visited[v] = true;
+//         for(int e : adj.get(v)){
+//             if(!visited[e]){
+//                 if(dfs(adj, visited, e, v)) return true;
+//             }
+//             else if(e != parent) return true;
+//         }
+//         return false;
+//     }
     
+//     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+//         // Code here
+//         boolean visited[] = new boolean[V];
+//         for(int i=0; i<V; i++){
+//             if(!visited[i]){
+//                 if(dfs(adj, visited, i, -1)) return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// BFS Based Approach
+class Solution {
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        boolean visited[] = new boolean[V];
+        boolean vis[] = new boolean[V];
+        int parents[] = new int[V];
+        Arrays.fill(parents, -1);
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        
         for(int i=0; i<V; i++){
-            if(!visited[i]){
-                if(dfs(adj, visited, i, -1)) return true;
+            if(!vis[i]){
+                q.offer(i);
+                vis[i] = true;
+                parents[i] = -1;
+                while(!q.isEmpty()){
+                    int node = q.poll();
+                    for(int it : adj.get(node)){
+                        if(!vis[it]){
+                            vis[it] = true;
+                            q.offer(it);
+                            parents[it] = node;
+                        }else if(parents[node] != it) return true;
+                    }
+                }       
             }
         }
         return false;
