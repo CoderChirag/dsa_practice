d054c4bad45043be934b0c35d76d3017102b2b6f
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2022-12-29 12:00:54 +0530

    Added solution - LeetHub

---
+++ Articulation Point - II - GFG/articulation-point-ii.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            int[] ans = obj.articulationPoints(V, adj);
+            for(int i: ans)
+                System.out.print(i + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to return Breadth First Traversal of given graph.
+    public void dfs(ArrayList<ArrayList<Integer>> adj, int u, int parent, int cnt, boolean vis[], int disc[], int low[], boolean isAP[]){
+        disc[u] = low[u] = cnt;
+        vis[u] = true;
+        int cntForRoot = 0;
+        for(int v : adj.get(u)){
+            if(!vis[v]){
+                dfs(adj, v, u, cnt+1, vis, disc, low, isAP);
+                cntForRoot++;
+                low[u] = Math.min(low[u], low[v]);
+                if(parent != -1 && low[v] >= disc[u]) isAP[u] = true;
+            }else if(v != parent){
+                low[u] = Math.min(low[u], disc[v]);
+            }
+        }
+        
+        if(parent == -1 && cntForRoot > 1) isAP[u] = true;
+    }
+    
+    public int[] articulationPoints(int V,ArrayList<ArrayList<Integer>> adj)
+    {
+        // Code here
+        boolean vis[] = new boolean[V];
+        int disc[] = new int[V];
+        int low[] = new int[V];
+        boolean isAP[] = new boolean[V];
+        ArrayList<Integer> al = new ArrayList<>();
+        
+        Arrays.fill(disc, -1);
+        Arrays.fill(low, -1);
+        
+        for(int i=0; i<V; i++){
+            if(!vis[i])
+                dfs(adj, i, -1, 1, vis, disc, low, isAP);
+        }
+        
+        for(int i=0; i<V; i++){
+            if(isAP[i]) al.add(i);
+        }
+        
+        if(al.size() > 0){
+            int res[] = new int[al.size()];
+            for(int i=0; i<al.size(); i++) res[i] = al.get(i);
+            return res;
+        }
+        
+        return new int[]{-1};
+    }
+}
