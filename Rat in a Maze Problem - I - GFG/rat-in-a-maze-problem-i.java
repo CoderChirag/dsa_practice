5d6f2619509cbd8eb0e3978ee557d74e932564a0
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-01-05 10:56:21 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -35,26 +35,61 @@ class Rat {
 // m is the given matrix and n is the order of matrix
 
 // Time Complexity: O(4^(N^2)) As we are making 4 calls from each of the N^2 cells of matrix
+// class Solution {
+//     public static void findPathRec(int[][] m, int n, int i, int j, ArrayList<String> res, String curr){
+//         if(i<0 || i>=n || j<0 || j>=n || m[i][j] == 0) return;
+//         if(i == n-1 && j == n-1){
+//             res.add(curr);
+//             return;
+//         }
+        
+//         m[i][j] = 0;
+//         findPathRec(m, n, i-1, j, res, curr+"U");
+//         findPathRec(m, n, i, j+1, res, curr+"R");
+//         findPathRec(m, n, i, j-1, res, curr+"L");
+//         findPathRec(m, n, i+1, j, res, curr+"D");
+//         m[i][j] = 1;
+//     }
+    
+//     public static ArrayList<String> findPath(int[][] m, int n) {
+//         // Your code here
+//         ArrayList<String> res = new ArrayList<>();
+//         findPathRec(m, n, 0, 0, res, "");
+//         return res;
+//     }
+// }
+
+// Time Complexity: O(3^(N^2)) As there are N^2 cells and from each cell there are 3 unvisited neighbouring cells
+// (Because, one of the cell is already visited every time)
 class Solution {
+    // Traverse in order Down->Left->Right->Up (so as to get the sorted paths)
+    static int dx[] = new int[]{1, 0, 0, -1};
+    static int dy[] = new int[]{0, -1, 1, 0};
+    static String key[] = new String[]{"D", "L", "R", "U" };
+    
+    public static boolean isSafe(int[][] m, int n, int i, int j){
+        return (i>=0 && i<n && j>=0 && j<n && m[i][j] == 1);
+    }
+    
     public static void findPathRec(int[][] m, int n, int i, int j, ArrayList<String> res, String curr){
-        if(i<0 || i>=n || j<0 || j>=n || m[i][j] == 0) return;
         if(i == n-1 && j == n-1){
             res.add(curr);
             return;
-        }
-        
+        };
         m[i][j] = 0;
-        findPathRec(m, n, i-1, j, res, curr+"U");
-        findPathRec(m, n, i, j+1, res, curr+"R");
-        findPathRec(m, n, i, j-1, res, curr+"L");
-        findPathRec(m, n, i+1, j, res, curr+"D");
+        for(int k=0; k<4; k++){
+            int x = i + dx[k], y = j + dy[k];
+            if(isSafe(m, n, x, y)) findPathRec(m, n, x, y, res, curr+key[k]);
+        }
         m[i][j] = 1;
     }
     
     public static ArrayList<String> findPath(int[][] m, int n) {
         // Your code here
         ArrayList<String> res = new ArrayList<>();
-        findPathRec(m, n, 0, 0, res, "");
+        if(m[0][0] == 1){
+            findPathRec(m, n, 0, 0, res, "");
+        }
         return res;
     }
 }
