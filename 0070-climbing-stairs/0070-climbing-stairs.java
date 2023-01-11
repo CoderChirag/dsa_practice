fd28f85dd90a1ead3501393dd8852741a8c92e24
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-01-11 16:01:25 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (75.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,23 +1,34 @@
-// Brute Force - Time Limit Exceeded
+// Brute Force - Recursion (Time Limit Exceeded) 
 // class Solution {
-//     public int countWays(int n, int i){
-//         if(i > n) return 0;
-//         if(i == n) return 1;
-//         return countWays(n, i+1) + countWays(n, i+2);
-//     }
 //     public int climbStairs(int n) {
-//         return countWays(n, 0);
+//         if(n == 0 || n == 1) return 1;
+        
+//         return climbStairs(n-1) + climbStairs(n-2); 
 //     }
 // }
 
+// 1D DP
+// class Solution {
+//     public int climbStairs(int n) {
+//         int a[] = new int[n+1];
+//         a[0] = 1;
+//         a[1] = 1;
+//         for(int i=2; i<=n; i++){
+//             a[i] = a[i-1] + a[i-2];
+//         }
+//         return a[n];
+//     }
+// }
+
+// DP - Without extra space
 class Solution {
     public int climbStairs(int n) {
-        int a = 1, b = 1, c = 1;
-        for(int i=2; i<=n; i++){
-            c = a+b;
+        int a = 1, b = 1, res = 1;
+        for(int i=1; i<n; i++){
+            res = a+b;
             a = b;
-            b = c;
+            b = res;
         }
-        return c;
+        return res;
     }
 }
