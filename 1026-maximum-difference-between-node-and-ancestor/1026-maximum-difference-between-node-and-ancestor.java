d1b44eaecf355c019e37cf266363af672f966666
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-01-11 15:11:57 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (46.66%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -15,23 +15,17 @@
  */
 class Solution {
     
-    int res = Integer.MIN_VALUE;
-    
-    public void maxAncestorDiffRec(TreeNode root, int currMin, int currMax){
-        if(root == null) return;
+    public int maxAncestorDiffRec(TreeNode root, int currMin, int currMax){
+        if(root == null) return currMax - currMin;
         
-        int max = Math.max(Math.abs(root.val-currMin), Math.abs(root.val-currMax));
-        res = Math.max(res, max);
         currMin = Math.min(currMin, root.val);
         currMax = Math.max(currMax, root.val);
-        maxAncestorDiffRec(root.left, currMin, currMax);
-        maxAncestorDiffRec(root.right, currMin, currMax);
-
+        int ls = maxAncestorDiffRec(root.left, currMin, currMax);
+        int rs = maxAncestorDiffRec(root.right, currMin, currMax);
+        return Math.max(ls, rs);
     }
     
     public int maxAncestorDiff(TreeNode root) {
-        res = Integer.MIN_VALUE;
-        maxAncestorDiffRec(root, root.val, root.val);
-        return res;
+        return maxAncestorDiffRec(root, root.val, root.val);
     }
 }
