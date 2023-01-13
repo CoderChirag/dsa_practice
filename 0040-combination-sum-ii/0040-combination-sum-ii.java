799419379da5d05f0ceb5b319d337aa0067b9788
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-01-13 13:58:01 +0530

    Time: 3 ms (87.57%), Space: 42.9 MB (60.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,48 +1,34 @@
 class Solution {
     
-    private void backtrack(LinkedList<Integer> comb,
+    private void backtrack(int[] candidates,
+                           LinkedList<Integer> comb,
                            int target, int currIndex,
-                           List<int[]> freqList,
                            List<List<Integer>> res) {
 
         if (target == 0) {
             res.add(new ArrayList<Integer>(comb));
             return;
-
         }
 
-        for (int i = currIndex; i < freqList.size(); i++) {
-            int[] entry = freqList.get(i);
-            int cand = entry[0], freq = entry[1];
+        for (int i = currIndex; i < candidates.length; i++) {
+            if(i > currIndex && candidates[i] == candidates[i-1]) continue;
+            
+            int cand = candidates[i];
 
-            if (freq > 0 && target >= cand){
+            if (target >= cand){
                 comb.addLast(cand);
-                freqList.set(i, new int[]{cand, freq - 1});
-                backtrack(comb, target - cand, i, freqList, res);
-                freqList.set(i, new int[]{cand, freq});
+                backtrack(candidates, comb, target - cand, i+1, res);
                 comb.removeLast();
             }
         }
     }
     
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        
         List<List<Integer>> res = new ArrayList<>();
-
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        for (int cand : candidates) {
-            if (freqMap.containsKey(cand))
-                freqMap.put(cand, freqMap.get(cand) + 1);
-            else
-                freqMap.put(cand, 1);
-        }
-
-        // convert the counter table to a list of (num, count) tuples
-        List<int[]> freqList = new ArrayList<>();
-        freqMap.forEach((key, value) -> {
-            freqList.add(new int[]{key, value});
-        });
-
-        backtrack(new LinkedList<>(), target, 0, freqList, res);
+        
+        backtrack(candidates, new LinkedList<>(), target, 0, res);
         return res;
     }
 }
