242a055b18081c8b0c6f304f19cffd06a98a9c6e
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-01-13 16:37:36 +0530

    Time: 10 ms (73.99%), Space: 56.1 MB (57.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,16 +1,43 @@
+// Backtracking - O(N*(2^N))
+// class Solution {
+    
+//     private boolean isPalindrome(String s){
+//         int l = 0, r = s.length()-1;
+//         while(l < r){
+//             if(s.charAt(l) != s.charAt(r)) return false;
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+    
+//     private void backtrack(String s, int currInd, LinkedList<String> comb, List<List<String>> res){
+//         if(currInd == s.length()){
+//             res.add(new ArrayList<>(comb));
+//             return;
+//         }
+        
+//         for(int i=currInd; i<s.length(); i++){
+//             String ss = s.substring(currInd, i+1);
+//             if(isPalindrome(ss)){
+//                 comb.addLast(ss);
+//                 backtrack(s, i+1, comb, res);
+//                 comb.removeLast();
+//             }
+//         }
+//     }
+    
+//     public List<List<String>> partition(String s) {
+//         List<List<String>> res = new ArrayList<>();
+//         backtrack(s, 0, new LinkedList<>(), res);
+//         return res;
+//     }
+// }
+
+
+// Backtracking with DP - O(2^N)
 class Solution {
-    
-    private boolean isPalindrome(String s){
-        int l = 0, r = s.length()-1;
-        while(l < r){
-            if(s.charAt(l) != s.charAt(r)) return false;
-            l++;
-            r--;
-        }
-        return true;
-    }
-    
-    private void backtrack(String s, int currInd, LinkedList<String> comb, List<List<String>> res){
+    private void backtrack(String s, int currInd, LinkedList<String> comb, List<List<String>> res, boolean[][] dp){
         if(currInd == s.length()){
             res.add(new ArrayList<>(comb));
             return;
@@ -18,17 +45,20 @@ class Solution {
         
         for(int i=currInd; i<s.length(); i++){
             String ss = s.substring(currInd, i+1);
-            if(isPalindrome(ss)){
+            if((s.charAt(currInd) == s.charAt(i)) && (i-currInd <= 2 || dp[currInd+1][i-1])){
+                dp[currInd][i] = true;
                 comb.addLast(ss);
-                backtrack(s, i+1, comb, res);
+                backtrack(s, i+1, comb, res, dp);
                 comb.removeLast();
             }
         }
     }
     
     public List<List<String>> partition(String s) {
+        int n = s.length();
         List<List<String>> res = new ArrayList<>();
-        backtrack(s, 0, new LinkedList<>(), res);
+        boolean dp[][] = new boolean[n][n];
+        backtrack(s, 0, new LinkedList<>(), res, dp);
         return res;
     }
 }
