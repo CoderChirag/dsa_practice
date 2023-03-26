f0579ee9816634bbb8ed28cffaf9ce34d60c6797
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-03-26 20:27:35 +0530

    Time: 20 ms (90.14%), Space: 42.9 MB (33.45%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,46 +1,41 @@
 class Solution {
-    public boolean rabinKarp(String s, String pat){
-        int q = 101, d = 256;
-        int n = s.length(), m = pat.length();
-        int h = 1;
-        for(int i=1; i<m; i++) h = (h*d)%q;
+    private int BASE = 256, MOD = 101; // BASE = d, MOD = q
+    
+    public boolean rabin_karp(String a, String b){
+        int n = a.length(), m = b.length();
+        if(n < m) return false;
+        int pow = 1; // pow = h, i.e, (d^(m-1))%q
+        for(int i=1; i<m; i++){
+            pow = (pow*BASE)%MOD;
+        }
         
-        int p = 0, t = 0;
+        int pattern_hash = 0, text_hash = 0;
         for(int i=0; i<m; i++){
-            p = (p*d + pat.charAt(i))%q;
-            t = (t*d + s.charAt(i))%q;
+            pattern_hash = (pattern_hash*BASE + b.charAt(i))%MOD;
+            text_hash = (text_hash*BASE + a.charAt(i))%MOD;
         }
         
         for(int i=0; i<n-m+1; i++){
-            if(p == t){
-                boolean flag = true;
-                for(int j=0; j<m; j++){
-                    if(s.charAt(i+j) != pat.charAt(j)){
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag) return true;
-            }
+            if(text_hash == pattern_hash && a.substring(i, i+m).equals(b)) return true;
+            
             if(i < n-m){
-                t = (d*(t - s.charAt(i)*h) + s.charAt(i+m))%q;
-                if(t < 0) t += q;
+                text_hash = (BASE*(text_hash - a.charAt(i)*pow) + a.charAt(i+m))%MOD;
+                if(text_hash < 0) text_hash += MOD;
             }
         }
         return false;
     }
     
     public int repeatedStringMatch(String a, String b) {
-        if(a == b) return 1;
-        int count = 1;
-        String source = a;
-        while(source.length() < b.length()){
-            count++;
-            source += a;
+        if(a.equals(b)) return 1;
+        int res = 1;
+        String str = a;
+        while(str.length() < b.length()){
+            res++;
+            str = str+a;
         }
-        if(a == b) return count;
-        if(rabinKarp(source, b)) return count;
-        if(rabinKarp(source+a, b)) return count+1;
+        if(rabin_karp(str, b)) return res;
+        if(rabin_karp(str+a, b))  return res+1;
         return -1;
     }
 }
