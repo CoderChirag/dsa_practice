83614ce1506f36efaf2a39b996559116d823b5f5
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-04-27 12:10:09 +0530

    Time: 21 ms (58.94%), Space: 42.9 MB (77.75%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0; i<n; i++){
-            long target1 = target - nums[i];
+            long target1 = target-nums[i];
             for(int j=i+1; j<n; j++){
-                long req = target1 - nums[j];
+                long target2 = target1-nums[j];
                 int l = j+1, r = n-1;
                 while(l < r){
-                    long sum = nums[l] + nums[r];
-                    if(sum < req) l++;
-                    else if(sum > req) r--;
-                    else{
-                        List<Integer> quad = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                    if(nums[l] + nums[r] == target2){
+                        ArrayList<Integer> quad = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        
                         res.add(quad);
                         
-                        //                     Handling Duplicates
-                    while(l < r && nums[l] == quad.get(2)) l++;
-                    while(l < r && nums[r] == quad.get(3)) r--;
-                    }
-                
+                        while(l < r && nums[l] == quad.get(2))l++;
+                        while(l < r && nums[r] == quad.get(3)) r--;
+                        
+                    }else if(nums[l] + nums[r] < target2)l++;
+                    else r--;
                 }
-                while(j+1 < n && nums[j+1] == nums[j]) j++;
+                while(j < n-1 && nums[j] == nums[j+1]) j++;
             }
-            while(i+1 < n && nums[i+1] == nums[i]) i++;
+            while(i < n-1 && nums[i] == nums[i+1]) i++;
         }
         return res;
     }
