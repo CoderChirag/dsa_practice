d2c27cd95b1e5ef7cac6cb1998a86637cf3a4506
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-04-27 19:00:36 +0530

    Time: 58 ms (68.06%), Space: 50.3 MB (77.96%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,54 +1,36 @@
 class Solution {
-//     O(N^2) - Time Limit Exceeded
-    // public int reversePairs(int[] nums) {
-    //     int n = nums.length, cnt = 0;
-    //     for(int i=0; i<n; i++){
-    //         for(int j=i+1; j<n; j++){
-    //             if(nums[j] < (double)nums[i]/2.0) cnt++;
-    //         }
-    //     }
-    //     return cnt;
-    // }
     
-//  Modified Merge Sort - O(N*logN) Time
     public int merge(int nums[], int l, int mid, int r){
-        int temp[] = new int[r-l+1];
+        int temp[] = new int[(r-l+1)];
         int i = l, j = mid+1, k = 0, count = 0;
-        for(; i<=mid; i++){
-            while(j <= r && nums[i] > 2 * (long)nums[j]) j++;
-            count += (j-(mid+1));
+        while(i <= mid && j <= r){
+            if(nums[i] > 2*(long)nums[j]){
+                count += mid-i+1;
+                j++;
+            }else i++;
         }
         i = l;
         j = mid+1;
         while(i <= mid && j <= r){
-            if(nums[j] <= nums[i]){  
-                temp[k++] = nums[j++];
-            }else{
-                temp[k++] = nums[i++];
-            }
+            if(nums[i] > nums[j]) temp[k++] = nums[j++];
+            else temp[k++] = nums[i++];
         }
-        while(i <= mid){
-            temp[k++] = nums[i++];
-        }
-        while(j <= r){
-            temp[k++] = nums[j++];
-        }
-        for(i=l,k=0; i<=r;i++,k++){
-            nums[i] = temp[k];
-        }
-        return count;
+        while(i <=  mid) temp[k++] = nums[i++];
+        while(j <= r) temp[k++] = nums[j++];
         
+        for(k=0; k<(r-l+1); k++) nums[k+l] = temp[k];
+        return count;
     }
-
+    
     public int mergeSort(int[] nums, int l, int r){
-        int cnt = 0;
+        int count = 0;
         if(l < r){
             int mid = (l+r)/2;
-            cnt += mergeSort(nums, l, mid);
-            cnt += mergeSort(nums, mid+1, r);
-            cnt += merge(nums, l, mid, r);
+            count += mergeSort(nums, l, mid);
+            count += mergeSort(nums, mid+1, r);
+            count += merge(nums, l, mid, r);
         }
-        return cnt;
+        return count;
     }
     
     public int reversePairs(int[] nums) {
