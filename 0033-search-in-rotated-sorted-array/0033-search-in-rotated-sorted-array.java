8ba9e8b40840c02c8c5e643b879b6b617f76e578
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-04-29 17:00:49 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (28.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,10 +5,10 @@ class Solution {
             int mid = (l+r)/2;
             if(nums[mid] == target) return mid;
             else if(nums[l] <= nums[mid]){  // Left Array is Sorted
-                if(nums[l] <= target && nums[mid] >= target) r = mid-1;
+                if(nums[l] <= target && nums[mid] > target) r = mid-1;
                 else l = mid+1;
             }else{ // Left array is unsorted and Right array is Sorted
-                if(nums[mid] <= target && nums[r] >= target) l = mid+1;
+                if(nums[mid] < target && nums[r] >= target) l = mid+1;
                 else r = mid-1;
             }
         }
