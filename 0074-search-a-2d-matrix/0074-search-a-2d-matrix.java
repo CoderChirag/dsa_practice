ba18da140307c69d55550a93cb346d4d8a46eda4
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-05-02 19:43:54 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (47.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        int l = 0, r = (m*n)-1;
-        while(l <= r){
-            int mid = (l + r)/2;
-            int i = mid/n, j = mid%n;
-            if(matrix[i][j] == target) return true;
-            else if(matrix[i][j] < target) l = mid+1;
-            else r = mid-1;
+        int min_row = 0, max_row = m-1;
+        while(min_row <= max_row){
+            int mid_row = (min_row + max_row)/2;
+            if(target >= matrix[mid_row][0] && target <= matrix[mid_row][n-1]){
+                int min_col = 0, max_col = n-1;
+                while(min_col <= max_col){
+                    int mid_col = (min_col + max_col)/2;
+                    if(matrix[mid_row][mid_col] == target) return true;
+                    else if(matrix[mid_row][mid_col] < target) min_col = mid_col+1;
+                    else max_col = mid_col-1;
+                }
+                return false;
+            }else if(target < matrix[mid_row][0]) max_row = mid_row-1;
+            else if(target > matrix[mid_row][n-1]) min_row = mid_row + 1;
         }
         return false;
     }
