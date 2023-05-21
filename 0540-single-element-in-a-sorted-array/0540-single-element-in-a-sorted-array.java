37d95e1373d31009692c2b9f7877830f42b908a8
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-05-21 23:53:28 +0530

    Time: 0 ms (100.00%), Space: 50.6 MB (5.26%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,10 +4,9 @@ class Solution {
         int l = 0, r = n-1;
         while(l <= r){
             int mid = (l+r)/2;
-            if(mid == 0 || mid == n-1) return nums[mid];
-            if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) return nums[mid];
-            else if((mid%2 == 0 && nums[mid-1] == nums[mid]) || (mid%2 != 0 && nums[mid+1] == nums[mid])) r = mid-1;
-            else l = mid+1;
+            if((mid == 0 || nums[mid-1] != nums[mid]) && (mid == n-1 || nums[mid+1] != nums[mid])) return nums[mid];
+            else if((mid%2 != 0 && nums[mid-1] == nums[mid]) || (mid%2 == 0 && nums[mid+1] == nums[mid])) l = mid+1;
+            else r = mid-1;
         }
         return -1;
     }
