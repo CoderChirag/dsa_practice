e1c5938a4b8ac32516827f7dfb08dd57977cfaf1
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2023-12-04 22:47:48 +0530

    Time: 56 ms (47.42%), Space: 42.9 MB (45.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -10,11 +10,14 @@
 // }
 
 function rob(nums: number[]): number {
-    let dp = new Array(nums.length+1).fill(-1);
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
+    if(nums.length === 1) return nums[0];
+    let prev2 = nums[0];
+    let prev = Math.max(nums[0], nums[1]);
+    let curr = prev;
     for(let i=2; i<nums.length; i++){
-        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        curr = Math.max(nums[i] + prev2, prev);
+        prev2 = prev;
+        prev = curr;
     }
-    return dp[nums.length-1];
+    return curr;
 };
